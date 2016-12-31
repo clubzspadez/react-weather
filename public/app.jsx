f2bd7a76cf483f7077d8e641fca296b3024e5d0a
Jonathan Rodriguez <clubzspadez@gmail.com>
2016-12-30 19:07:51 -0500

    Added webpack

--- public/app.jsx
+++ public/app.jsx
@@ -1,85 +1,76 @@
 var GreeterMessage = React.createClass({
-  render: function(){
-    var name = this.props.name;
-    var message = this.props.message;
-    return(
-      <div>
-        <h1>Hello {name}!</h1>
-        <p>{message}</p>
-      </div>
-    );
-  }
+    render: function() {
+        var name = this.props.name;
+        var message = this.props.message;
+        return (
+            <div>
+                <h1>Hello {name}!</h1>
+                <p>{message}</p>
+            </div>
+        );
+    }
 });
 
 var GreeterForm = React.createClass({
-  onFormSubmit: function(e){
-    e.preventDefault();
+    onFormSubmit: function(e) {
+        e.preventDefault();
 
-    var updates = {};
-    var name = this.refs.name.value;
-    var message = this.refs.message.value;
+        var updates = {};
+        var name = this.refs.name.value;
+        var message = this.refs.message.value;
 
-
-    if(name.length > 0){
-      this.refs.name.vale = '';
-      updates.name = name;
+        if (name.length > 0) {
+            this.refs.name.vale = '';
+            updates.name = name;
+        }
+        if (message.length > 0) {
+            this.refs.message.vale = '';
+            updates.message = message;
+        }
+        this.props.onNewData(updates);
+    },
+    render: function() {
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <div>
+                    <input type="text" ref="name"/>
+                </div>
+                <div>
+                    <textarea ref="message"></textarea>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
+            </form>
+        );
     }
-    if(message.length > 0){
-      this.refs.message.vale = '';
-      updates.message = message;
-    }
-    this.props.onNewData(updates);
-  },
-  render: function(){
-    return(
-      <form onSubmit={this.onFormSubmit}>
-        <div>
-          <input type="text" ref="name"/>
-        </div>
-        <div>
-          <textarea ref="message"></textarea>
-        </div>
-        <div>
-          <button>Submit</button>
-        </div>
-      </form>
-    );
-  }
 });
 
 var Greeter = React.createClass({
-  getDefaultProps: function(){
-    return{
-      message: 'This is from a component!'
+    getDefaultProps: function() {
+        return {message: 'This is from a component!'}
+    },
+    getInitialState: function() {
+        return {name: this.props.name, message: this.props.message};
+    },
+    handleNewData: function(updates) {
+        this.setState(updates);
+    },
+    render: function() {
+        var name = this.state.name;
+        var message = this.state.message;
+
+        return (
+            <div>
+                <GreeterMessage name={name} message={message}/>
+                <GreeterForm onNewData={this.handleNewData}/>
+
+            </div>
+        );
     }
-  },
-  getInitialState: function(){
-    return{
-      name: this.props.name,
-      message:  this.props.message
-    };
-  },
-  handleNewData: function(updates){
-    this.setState(updates);
-  },
-  render: function(){
-    var name = this.state.name;
-    var message = this.state.message;
-
-    return (
-      <div>
-        <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewData={this.handleNewData}/>
-
-      </div>
-    );
-  }
 });
 
-
-var name ='Jonathan';
+var name = 'Jonathan';
 
 ReactDOM.render(
-  <Greeter name={name}/>,
-  document.getElementById('app')
-);
+    <Greeter name={name}/>, document.getElementById('app'));
