5c849e5b674b053016c1885c50621ca619fb493c
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-01-12 20:26:51 -0500

    removed old files and adding routing

--- public/app.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Greeter = require('Greeter');
-
-
-var name = 'Jonathan';
-
-ReactDOM.render(
-    <Greeter name={name}/>, document.getElementById('app'));
